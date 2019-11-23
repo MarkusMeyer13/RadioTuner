8d3905ef8454e28eb933c139f70d1515d6c28caf
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2019-11-23 09:35:54 +0100

    Using System.Timers

--- RadioTuner/Form1.Designer.cs
+++ RadioTuner/Form1.Designer.cs
@@ -35,10 +35,12 @@
             this.pbMagicEyeBak = new System.Windows.Forms.PictureBox();
             this.pbMagicEyeLeft = new System.Windows.Forms.PictureBox();
             this.pbMagicEyeRigth = new System.Windows.Forms.PictureBox();
+            this.pbPointer = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbScala)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeBak)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeLeft)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeRigth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPointer)).BeginInit();
             this.SuspendLayout();
             // 
             // pbScala
@@ -46,7 +48,7 @@
             this.pbScala.Image = ((System.Drawing.Image)(resources.GetObject("pbScala.Image")));
             this.pbScala.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbScala.InitialImage")));
             this.pbScala.Location = new System.Drawing.Point(50, 133);
-            this.pbScala.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbScala.Margin = new System.Windows.Forms.Padding(2);
             this.pbScala.Name = "pbScala";
             this.pbScala.Size = new System.Drawing.Size(660, 52);
             this.pbScala.TabIndex = 0;
@@ -88,37 +90,48 @@
             this.pbMagicEyeLeft.BackColor = System.Drawing.Color.Lime;
             this.pbMagicEyeLeft.Location = new System.Drawing.Point(215, 68);
             this.pbMagicEyeLeft.Name = "pbMagicEyeLeft";
-            this.pbMagicEyeLeft.Size = new System.Drawing.Size(10, 50);
+            this.pbMagicEyeLeft.Size = new System.Drawing.Size(5, 50);
             this.pbMagicEyeLeft.TabIndex = 4;
             this.pbMagicEyeLeft.TabStop = false;
             // 
             // pbMagicEyeRigth
             // 
             this.pbMagicEyeRigth.BackColor = System.Drawing.Color.Lime;
-            this.pbMagicEyeRigth.Location = new System.Drawing.Point(535, 68);
+            this.pbMagicEyeRigth.Location = new System.Drawing.Point(540, 68);
             this.pbMagicEyeRigth.Name = "pbMagicEyeRigth";
-            this.pbMagicEyeRigth.Size = new System.Drawing.Size(10, 50);
+            this.pbMagicEyeRigth.Size = new System.Drawing.Size(5, 50);
             this.pbMagicEyeRigth.TabIndex = 5;
             this.pbMagicEyeRigth.TabStop = false;
+            // 
+            // pbPointer
+            // 
+            this.pbPointer.BackColor = System.Drawing.Color.Red;
+            this.pbPointer.Location = new System.Drawing.Point(275, 133);
+            this.pbPointer.Name = "pbPointer";
+            this.pbPointer.Size = new System.Drawing.Size(5, 50);
+            this.pbPointer.TabIndex = 6;
+            this.pbPointer.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(752, 379);
+            this.Controls.Add(this.pbPointer);
             this.Controls.Add(this.pbMagicEyeRigth);
             this.Controls.Add(this.pbMagicEyeLeft);
             this.Controls.Add(this.pbMagicEyeBak);
             this.Controls.Add(this.btnLeft);
             this.Controls.Add(this.btnRight);
             this.Controls.Add(this.pbScala);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pbScala)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeBak)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeLeft)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeRigth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPointer)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -131,6 +144,7 @@
         private System.Windows.Forms.PictureBox pbMagicEyeBak;
         private System.Windows.Forms.PictureBox pbMagicEyeLeft;
         private System.Windows.Forms.PictureBox pbMagicEyeRigth;
+        private System.Windows.Forms.PictureBox pbPointer;
     }
 }
 
--- RadioTuner/Form1.cs
+++ RadioTuner/Form1.cs
@@ -13,7 +13,7 @@ namespace RadioTuner
 {
     public partial class Form1 : Form
     {
-        private Timer timer;
+        private System.Timers.Timer timer = new System.Timers.Timer();
         private int upDown;
         private int counter;
 
@@ -24,12 +24,73 @@ namespace RadioTuner
 
         private void btn_MouseDown(object sender, MouseEventArgs e)
         {
-            Trace.WriteLine("btn_MouseDown");
+            Trace.WriteLine("btn_MouseDown Start - " + this.upDown.ToString());
+            if (sender == this.btnLeft)
+            {
+                this.upDown = -1;
+            }
+            else if (sender == this.btnRight)
+            {
+                this.upDown = 1;
+            }
+            this.timer.Interval = 100;
+            this.timer.Elapsed += Timer_Elapsed;
+            this.timer.Enabled = true;
+            Trace.WriteLine("btn_MouseDown End - " + this.upDown.ToString());
         }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Trace.WriteLine("elapsed - " + counter.ToString());
+            this.counter += 1;
+
+            if (this.counter >= 10 && this.counter < 25)
+            {
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+            }
+            else if (this.counter >= 25 && this.counter < 50)
+            {
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+
+            }
+            else if (this.counter >= 50)
+            {
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+                this.Invoke((Func<int, bool>)MovePointer, this.upDown);
+
+            }
+        }
+
+        private bool MovePointer(int count)
+        {
+            Trace.WriteLine("MovePointer");
+
+            if ((this.pbPointer.Location.X > this.pbScala.Location.X) && (this.pbPointer.Location.X < this.pbScala.Location.X + this.pbScala.Size.Width))
+            {
+                this.pbPointer.Location = new Point(this.pbPointer.Location.X + count, this.pbPointer.Location.Y);
+            }
+            return true;
+        }
+
 
         private void btn_MouseUp(object sender, MouseEventArgs e)
         {
             Trace.WriteLine("btn_MouseUp");
+            if (this.timer != null)
+            {
+                this.timer.Stop();
+                this.timer.Enabled = false;
+                Trace.WriteLine("btn_MouseUpc - Timer stopped");
+            }
 
         }
 
