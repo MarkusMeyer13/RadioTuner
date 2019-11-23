0479468a506f7384b681437fb101d95c55fde6e1
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2019-11-23 08:36:15 +0100

    Add project files.

---
+++ RadioTuner/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RadioTuner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbScala = new System.Windows.Forms.PictureBox();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.pbMagicEyeBak = new System.Windows.Forms.PictureBox();
+            this.pbMagicEyeLeft = new System.Windows.Forms.PictureBox();
+            this.pbMagicEyeRigth = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScala)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeBak)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeRigth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbScala
+            // 
+            this.pbScala.Image = ((System.Drawing.Image)(resources.GetObject("pbScala.Image")));
+            this.pbScala.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbScala.InitialImage")));
+            this.pbScala.Location = new System.Drawing.Point(50, 133);
+            this.pbScala.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbScala.Name = "pbScala";
+            this.pbScala.Size = new System.Drawing.Size(660, 52);
+            this.pbScala.TabIndex = 0;
+            this.pbScala.TabStop = false;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(423, 198);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = ">>";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_MouseDown);
+            this.btnRight.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(263, 198);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "<<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_MouseDown);
+            this.btnLeft.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_MouseUp);
+            // 
+            // pbMagicEyeBak
+            // 
+            this.pbMagicEyeBak.BackColor = System.Drawing.Color.DarkGreen;
+            this.pbMagicEyeBak.Location = new System.Drawing.Point(215, 68);
+            this.pbMagicEyeBak.Name = "pbMagicEyeBak";
+            this.pbMagicEyeBak.Size = new System.Drawing.Size(330, 50);
+            this.pbMagicEyeBak.TabIndex = 3;
+            this.pbMagicEyeBak.TabStop = false;
+            // 
+            // pbMagicEyeLeft
+            // 
+            this.pbMagicEyeLeft.BackColor = System.Drawing.Color.Lime;
+            this.pbMagicEyeLeft.Location = new System.Drawing.Point(215, 68);
+            this.pbMagicEyeLeft.Name = "pbMagicEyeLeft";
+            this.pbMagicEyeLeft.Size = new System.Drawing.Size(10, 50);
+            this.pbMagicEyeLeft.TabIndex = 4;
+            this.pbMagicEyeLeft.TabStop = false;
+            // 
+            // pbMagicEyeRigth
+            // 
+            this.pbMagicEyeRigth.BackColor = System.Drawing.Color.Lime;
+            this.pbMagicEyeRigth.Location = new System.Drawing.Point(535, 68);
+            this.pbMagicEyeRigth.Name = "pbMagicEyeRigth";
+            this.pbMagicEyeRigth.Size = new System.Drawing.Size(10, 50);
+            this.pbMagicEyeRigth.TabIndex = 5;
+            this.pbMagicEyeRigth.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 379);
+            this.Controls.Add(this.pbMagicEyeRigth);
+            this.Controls.Add(this.pbMagicEyeLeft);
+            this.Controls.Add(this.pbMagicEyeBak);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.pbScala);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbScala)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeBak)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagicEyeRigth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbScala;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.PictureBox pbMagicEyeBak;
+        private System.Windows.Forms.PictureBox pbMagicEyeLeft;
+        private System.Windows.Forms.PictureBox pbMagicEyeRigth;
+    }
+}
+
---
+++ RadioTuner/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioTuner
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int upDown;
+        private int counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            Trace.WriteLine("btn_MouseDown");
+        }
+
+        private void btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            Trace.WriteLine("btn_MouseUp");
+
+        }
+
+    }
+}
